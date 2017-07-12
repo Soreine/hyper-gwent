7911d1f3564ae13fb3bce6b479197e0cebc018c4
Gabin <hello@gabinaureche.com>
2017-07-12 23:05:00 +0200

    Use external CSS and shadow DOM

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
           {
             loader: 'css-loader',
             options: {
               modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              localIdentName: '[local]',
             },
           },
         ],
--- webpack.config.website.js
+++ webpack.config.website.js
@@ -16,12 +16,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
           {
             loader: 'css-loader',
             options: {
               modules: true,
-              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              localIdentName: '[local]',
             },
           },
         ],
