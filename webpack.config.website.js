fe9601137e144c5a3ba102f3c3635646466ea530
Gabin <hello@gabinaureche.com>
2017-07-12 23:34:09 +0200

    Remove configuration duplication

--- webpack.config.website.js
+++ webpack.config.website.js
@@ -1,35 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const baseConfig = require('./webpack.config');
 
-module.exports = {
+module.exports = Object.assign({}, baseConfig, {
   entry: path.resolve(__dirname, './src/website'),
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'website.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[local]',
-            },
-          },
-        ],
-      },
-    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Hyper Gwent',
     }),
   ],
-};
+});
