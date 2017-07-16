ed0e1216fa6af75bf7d03f4ebfec654cee949965
Gabin <hello@gabinaureche.com>
2017-07-16 21:36:23 +0200

    Highlight found matches

--- src/extension/content.js
+++ src/extension/content.js
@@ -1,1 +1,30 @@
-console.log('Hey, this is Hyper Gwent! Hooray!');
+/* global window */
+
+import findAllMatches from '../core/findAllMatches';
+import replaceMatches from '../core/replaceMatches';
+import DICTIONNARY from '../core/dictionary';
+
+const walker = window.document.createTreeWalker(
+  window.document.body,
+  window.NodeFilter.SHOW_TEXT,
+);
+const nodes = [];
+
+while (walker.nextNode()) {
+  const node = walker.currentNode;
+  const matches = findAllMatches(DICTIONNARY, node.nodeValue);
+
+  if (matches.length) {
+    nodes.push({
+      node,
+      matches,
+    });
+  }
+}
+
+nodes.forEach(({ node, matches }) => {
+  const span = window.document.createElement('span');
+  span.innerHTML = replaceMatches(node.nodeValue, matches, match => `<span style="outline: 2px solid red;">${match.match}</span>`);
+
+  node.parentNode.replaceChild(span, node);
+});
