d072037d88bed597a62041c33aeec6390eecabc9
Soreine <soreine.plume@gmail.com>
2017-07-22 14:38:26 +0200

    Do not replace text in web page

--- src/extension/content.js
+++ src/extension/content.js
@@ -24,7 +24,7 @@ while (walker.nextNode()) {
 
 nodes.forEach(({ node, matches }) => {
   const span = window.document.createElement('span');
-  span.innerHTML = replaceMatches(node.nodeValue, matches, match => `<span style="outline: 2px solid red;">${match.entryValue}</span>`);
+  span.innerHTML = replaceMatches(node.nodeValue, matches, match => `<span style="outline: 2px solid red;">${node.nodeValue.slice(match.start, match.end)}</span>`);
 
   node.parentNode.replaceChild(span, node);
 });
