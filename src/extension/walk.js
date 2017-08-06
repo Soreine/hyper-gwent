9cdf17fbd8f226b88b8f1404a9414017ef277491
Soreine <soreine.plume@gmail.com>
2017-08-06 10:24:07 +0200

    Wrote content for home page

---
+++ src/extension/walk.js
@@ -0,0 +1,87 @@
+/* global window, document */
+
+import urlParse from 'url-parse';
+import findAllMatches from '../core/findAllMatches';
+import replaceMatches from '../core/replaceMatches';
+import tooltip from '../core/tooltip/index';
+import { CARDS } from '../core/data';
+import DICTIONARY from '../core/dictionary';
+
+const CLASSNAME = 'hyper-gwent-card-highlight';
+const CARD_NAME_ATTRIBUTE = 'data-card-name';
+const GWENTDB_TOOLTIP_ATTR = 'data-tooltip-url';
+const GWENTDB_HOSTNAME = 'www.gwentdb.com';
+
+// Walk the document and highlight cards
+function walk() {
+  const HOSTNAME = urlParse(window.location.href).hostname;
+
+  const walker = window.document.createTreeWalker(
+      window.document.body,
+      window.NodeFilter.SHOW_ELEMENT + window.NodeFilter.SHOW_TEXT,
+      // Filter out GwentDB tooltips
+    {
+      acceptNode(node) {
+        const TEXT_NODE = 3;
+        const ELEMENT_NODE = 1;
+
+        const {
+            FILTER_ACCEPT,
+            FILTER_REJECT,
+            FILTER_SKIP,
+          } = window.NodeFilter;
+
+          // Non GwentDB
+        if (HOSTNAME !== GWENTDB_HOSTNAME) {
+          return node.nodeType === TEXT_NODE
+            ? FILTER_ACCEPT
+            : FILTER_SKIP;
+        }
+
+          // on GwentDB, we skip existing tooltips
+        if (
+            node.nodeType === ELEMENT_NODE
+            && node.getAttribute(GWENTDB_TOOLTIP_ATTR)
+          ) {
+            // Skip this node and all its children
+          return FILTER_REJECT;
+        } else if (node.nodeType === TEXT_NODE) {
+          return FILTER_ACCEPT;
+        }
+        return FILTER_SKIP;
+      },
+    },
+    );
+
+  const nodes = [];
+
+  while (walker.nextNode()) {
+    const node = walker.currentNode;
+    const matches = findAllMatches(DICTIONARY, node.nodeValue);
+
+    if (matches.length) {
+      nodes.push({
+        node,
+        matches,
+      });
+    }
+  }
+
+  nodes.forEach(({ node, matches }) => {
+    const span = window.document.createElement('span');
+    span.innerHTML = replaceMatches(node.nodeValue, matches, match => `<span class="${CLASSNAME}" ${CARD_NAME_ATTRIBUTE}="${match.entryValue}" style="border-bottom: 1px dashed; padding-bottom: 0.1em">${node.nodeValue.slice(match.start, match.end)}</span>`);
+
+    node.parentNode.replaceChild(span, node);
+  });
+
+    // Add tooltips
+  const highlights = document.getElementsByClassName(CLASSNAME);
+  for (let i = 0; i < highlights.length; i += 1) {
+    const highlight = highlights[i];
+    const cardName = highlight.getAttribute(CARD_NAME_ATTRIBUTE);
+    const card = CARDS[cardName];
+    tooltip(card, highlight);
+  }
+}
+
+export default walk;
--- src/website/index.js
+++ src/website/index.js
@@ -2,31 +2,52 @@
 
 // eslint-disable-next-line no-unused-vars
 import { createElement } from 'jsx-dom';
-import tooltip from '../core/tooltip/index';
+import walk from '../extension/walk';
 
-const target = <span id="target" style="color: blue;">Aeromancy</span>;
-const app = (
+const REDDIT = 'https://www.reddit.com/r/gwent/';
+const GWENTDB = 'http://www.gwentdb.com/';
+const EXT_LINK = '#';
+
+const htmlPage = (
   /* eslint-disable max-len */
   <div>
-    <p>C makes it easy to shoot yourself in the foot; C++ makes it harder, but when you do, it blows away your whole leg.</p>
-    <p>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</p>
-    <p>This paragraph contains a card named {target}.</p>
-    <p>It's not a bug - it's an undocumented feature.</p>
-    <p>A good programmer is someone who always looks both ways before crossing a one-way street.</p>
-    <p>The perfect project plan is possible if one first documents a list of all the unknowns.</p>
+    <div className="logo">
+      <div className="logo-title">Hyper Gwent</div>
+      <div className="logo-subtitle">Community Chrome extension for GWENT®: The Witcher Card Game</div>
+    </div>
+    <div className="download-link">
+      <a href={EXT_LINK}>Add to Chrome</a>
+    </div>
+    <div className="description">
+      <p>
+        {'None of us cannot reasonably remember every possible Gwent card\'s name and effect. Browsing '}
+        <a href={REDDIT}>r/gwent</a>
+        {' and reading a deck guide on '}
+        <a href={GWENTDB}>GwentDB</a>
+        {' can be challenging if you don\'t know half the cards they\'re talking about.'}
+      </p>
+      <p>
+        {'Did you know that...'}
+        <ul>
+          <li>Xmen are not what they used to be ?</li>
+          <li>ADC does not stand for Attack Damage Carry ?</li>
+          <li>There <em>are</em> worst names for a dragon than Borkh ?</li>
+          <li>Gwent has nothing to do with cooking frogs, mushrooms, or anything remotely related to french cuisine ? 🍷</li>
+        </ul>
+      </p>
+      <p>
+        {'Whether you are new to the game, you want to keep up with the latest card changes,'}
+        {' or you can\'t remember 💩, you will enjoy the smart card tooltips from Hyper Gwent.'}
+      </p>
+      <p className="get-it-now">
+        <a href={EXT_LINK}>Get it now!</a>
+      </p>
+    </div>
   </div>
   /* eslint-enable max-len */
 );
-window.document.body.appendChild(app);
+window.document.body.appendChild(htmlPage);
 
-tooltip({
-  name: 'Aeromancy',
-  info: 'Play a Bronze or Silver Weather card from your Deck or Graveyard. Shuffle the others from your Deck back.',
-  variations: [
-    {
-      art: {
-        thumbnailImage: 'https://api.gwentapi.com/media/aeromancy-thumbnail.png',
-      },
-    },
-  ],
-}, target);
+
+// Launch extension within the page
+walk();
