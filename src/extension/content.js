25d5b55a6b99366bf12a64942957ecad4c8bcb2e
Soreine <soreine.plume@gmail.com>
2017-08-06 08:21:29 +0200

    Support GwentDB (don't double tooltips)

--- src/extension/content.js
+++ src/extension/content.js
@@ -1,5 +1,6 @@
 /* global window, document */
 
+import urlParse from 'url-parse';
 import findAllMatches from '../core/findAllMatches';
 import replaceMatches from '../core/replaceMatches';
 import tooltip from '../core/tooltip/index';
@@ -8,10 +9,48 @@ import DICTIONARY from '../core/dictionary';
 
 const CLASSNAME = 'hyper-gwent-card-highlight';
 const CARD_NAME_ATTRIBUTE = 'data-card-name';
+// Set of classname to limit the extension to
+const GWENTDB_TOOLTIP_ATTR = 'data-tooltip-url';
+const GWENTDB_HOSTNAME = 'www.gwentdb.com';
+
+const HOSTNAME = urlParse(window.location.href).hostname;
 
 const walker = window.document.createTreeWalker(
   window.document.body,
-  window.NodeFilter.SHOW_TEXT,
+  window.NodeFilter.SHOW_ELEMENT + window.NodeFilter.SHOW_TEXT,
+  // Filter out GwentDB tooltips
+  {
+    acceptNode(node) {
+      const TEXT_NODE = 3;
+      const ELEMENT_NODE = 1;
+
+      const {
+        FILTER_ACCEPT,
+        FILTER_REJECT,
+        FILTER_SKIP,
+      } = window.NodeFilter;
+
+      // Non GwentDB
+      if (HOSTNAME !== GWENTDB_HOSTNAME) {
+        if (node.nodeType === TEXT_NODE) {
+          return FILTER_ACCEPT;
+        }
+        return FILTER_SKIP;
+      }
+
+      // on GwentDB
+      if (
+        node.nodeType === ELEMENT_NODE
+        && node.getAttribute(GWENTDB_TOOLTIP_ATTR)
+      ) {
+        // Skip this node and all its children
+        return FILTER_REJECT;
+      } else if (node.nodeType === TEXT_NODE) {
+        return FILTER_ACCEPT;
+      }
+      return FILTER_SKIP;
+    },
+  },
 );
 const nodes = [];
 
