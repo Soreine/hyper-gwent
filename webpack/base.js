292b1d47226c7081755e89279ffe0c7cb6e8e96c
Soreine <soreine.plume@gmail.com>
2018-11-19 21:33:45 +0100

    Support Gwent font

--- webpack/base.js
+++ webpack/base.js
@@ -28,6 +28,19 @@ module.exports = {
             {
                 test: /\.svg$/,
                 loader: 'svg-inline-loader'
+            },
+            {
+                // Fonts
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
             }
         ]
     }
