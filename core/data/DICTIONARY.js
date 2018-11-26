fa0cca0f0b7713fd0a81b51c90da1b7bb78345f4
Soreine <soreine.plume@gmail.com>
2018-11-26 23:21:35 +0100

    Adapted old aliases

--- core/data/DICTIONARY.js
+++ core/data/DICTIONARY.js
@@ -2,6 +2,7 @@
 import pluralize from 'pluralize';
 import removeAccents from 'remove-accents';
 import CARD_LIST from './CARD_LIST';
+import ALIASES from './ALIASES';
 import { create } from '../dictionary';
 
 import type { Dictionary, Card } from '../types';
@@ -16,12 +17,12 @@ const DICTIONARY: Dictionary<CardID> = create(
         // Plural
         array.push([pluralize(cleanName), id]);
 
-        // TODO Aliases
-        // const aliases = ALIASES[name] || [];
-        // aliases.forEach(alias => {
-        //     array.push([alias, name]);
-        //     array.push([pluralize(alias), name]);
-        // });
+        // Aliases
+        const aliases = ALIASES[id] || [];
+        aliases.forEach(alias => {
+            array.push([alias, id]);
+            array.push([pluralize(alias), id]);
+        });
 
         return array;
     }, [])
