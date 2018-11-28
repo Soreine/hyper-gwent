57e0ce4a9b9ba4329256c72b5a8020e2f3007408
Soreine <soreine.plume@gmail.com>
2018-11-28 21:32:38 +0100

    Make aliases include original name

--- core/data/adapt-aliases.js
+++ core/data/adapt-aliases.js
@@ -1,19 +1,14 @@
 import ALIASES from './ALIASES';
 import CARD_LIST from './CARD_LIST';
 
-const NAME_IDS = {};
-CARD_LIST.forEach(card => {
-    NAME_IDS[card.name] = card.id;
-});
-
 const NEW = {};
 CARD_LIST.forEach(card => {
-    NEW[card.id] = [];
+    NEW[card.id] = [card.name];
 });
-Object.keys(ALIASES).forEach(name => {
-    const id = NAME_IDS[name];
+
+Object.keys(ALIASES).forEach(id => {
     if (id) {
-        NEW[id] = ALIASES[name];
+        NEW[id] = NEW[id].concat(ALIASES[id]);
     }
 });
 
