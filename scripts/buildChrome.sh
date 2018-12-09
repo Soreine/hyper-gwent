fb5ac9f84c096d0aaa6e9f4e2fb20d561c787535
Soreine <soreine.plume@gmail.com>
2018-12-09 11:49:40 +0100

    Fix loading of web extension images

--- scripts/buildChrome.sh
+++ scripts/buildChrome.sh
@@ -2,6 +2,7 @@
 set -o errexit
 set -o pipefail
 set -o nounset
+set -o xtrace
 # ----------
 
 DIST=./dist/chrome
@@ -9,6 +10,9 @@ DIST=./dist/chrome
 # Cleanup
 rm -rf $DIST
 mkdir -p $DIST
+
+# Copy manifest
+cp ./extension/manifest.json $DIST
 
 # Copy resources
 cp ./extension/icon*.png $DIST
