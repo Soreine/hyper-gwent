d17ad407c1496e490ad5476071473d3d8bfb7a73
Soreine <soreine.plume@gmail.com>
2019-04-16 21:37:42 +0200

    WIP: Popup made in react

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,8 @@
 // @flow
+/* @jsx h */
 /* global document */
 
+import { h, render, Component } from 'preact';
 import browser from 'webextension-polyfill';
 
 type Options = {
@@ -54,15 +56,11 @@ const UI = {
     }
 };
 
-let currentOptions = DEFAULT_OPTIONS;
-
 // Update the options, and propagate it to the UI and the storage
 async function mutateOptions(mutator: (prevOptions: Options) => void) {
-    log('mutating', { currentOptions });
     mutator(currentOptions);
     UI.updateForOptions(currentOptions);
     saveOptions(currentOptions);
-    log('saved', { currentOptions });
 }
 
 // Saves options to browser.storage.sync.
@@ -71,6 +69,12 @@ async function saveOptions({
     lowQualityArt,
     enabledDomains
 }: Options) {
+    if (!browser.storage) {
+        console.warn(
+            'Current context does not provide browser APIs. Skipping saving options'
+        );
+        return;
+    }
     await browser.storage.sync.set({
         shouldUnderline,
         lowQualityArt,
@@ -80,32 +84,114 @@ async function saveOptions({
 
 // Restores select box and checkbox state using the preferences
 // stored in browser.storage.
-async function loadOptions() {
-    // Use default value color = 'red' and likesColor = true.
-    const options = await browser.storage.sync.get(DEFAULT_OPTIONS);
-
-    currentOptions = options;
-    UI.updateForOptions(currentOptions);
+async function loadOptions(): Options {
+    if (!browser.storage) {
+        console.warn(
+            'Current context does not provide browser APIs. Skipping loading options'
+        );
+        return DEFAULT_OPTIONS;
+    }
+    return await browser.storage.sync.get(DEFAULT_OPTIONS);
 }
 
-document.addEventListener('DOMContentLoaded', loadOptions);
+// UI.shouldUnderline.addEventListener('change', () => {
+//     mutateOptions(options => {
+//         options.shouldUnderline = !options.shouldUnderline;
+//     });
+// });
+// UI.lowQualityArt.addEventListener('change', () => {
+//     mutateOptions(options => {
+//         options.lowQualityArt = !options.lowQualityArt;
+//     });
+// });
+// UI.enabledSwitch.addEventListener('click', () => {
+//     mutateOptions(options => {});
+// });
 
-UI.shouldUnderline.addEventListener('change', () => {
-    mutateOptions(options => {
-        options.shouldUnderline = !options.shouldUnderline;
+async function getCurrentUrl(): Promise<string> {
+    const tabs = await browser.tabs.query({
+        active: true,
+        currentWindow: true
     });
-});
-UI.lowQualityArt.addEventListener('change', () => {
-    mutateOptions(options => {
-        options.lowQualityArt = !options.lowQualityArt;
-    });
-});
-UI.enabledSwitch.addEventListener('click', () => {
-    mutateOptions(options => {});
-});
 
-console.log('Loaded popup.js');
-
-function log(...args) {
-    console.log(...args);
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    const activeTab = tabs[0];
+    console.log({ activeTab, url: activeTab.url });
+    return activeTab.url;
 }
+
+class OptionsPanel extends Component<
+    {},
+    { options: ?Options, currentUrl: string }
+> {
+    componentDidMount() {
+        setTimeout(async () => {
+            const options = await loadOptions();
+            const currentUrl = await getCurrentUrl();
+            this.setState({ options, currentUrl });
+        });
+    }
+
+    render({}, { options, currentUrl }) {
+        if (!options) {
+            return null;
+        }
+
+        const { shouldUnderline, lowQualityArt, enabledDomains } = options;
+
+        return (
+            <div>
+                <label>
+                    Enable Hyper Gwent on {currentUrl}
+                    <Switch
+                        checked={enabledDomains}
+                        onChange={newValue => {
+                            this.setState({
+                                options: {
+                                    ...options,
+                                    enabledDomains: newValue
+                                }
+                            });
+                        }}
+                    />
+                </label>
+
+                <label>
+                    <input type="checkbox" checked={shouldUnderline} />
+                    Underline card names
+                </label>
+
+                <label>
+                    <input type="checkbox" checked={lowQualityArt} />
+                    Use low quality card arts.
+                    <div className="hint">
+                        Tooltips may load faster if your internet connection is
+                        slow.
+                    </div>
+                </label>
+            </div>
+        );
+    }
+}
+
+function Switch({
+    checked,
+    onChange
+}: {
+    checked: boolean,
+    onChange: (newValue: boolean) => void
+}) {
+    return (
+        <div className="switch" id="switch" onClick={() => onChange(!checked)}>
+            <div
+                className={checked ? 'switch-pin switch-pin-on' : 'switch-pin'}
+                id="pin"
+            />
+            <div className="switch-label">Off</div>
+            <div className="switch-label">On</div>
+        </div>
+    );
+}
+
+render(<OptionsPanel />, document.body);
