8a29bcd5ee47db9f75380c10b85808e97b45361c
Soreine <soreine.plume@gmail.com>
2019-04-20 16:23:19 +0200

    Write more accurate test for dictionary entries conflicts.
    Conflicts that were detected are now removed.

--- core/data/__tests__/DICTIONARY.js
+++ core/data/__tests__/DICTIONARY.js
@@ -2,8 +2,11 @@
 import test from 'ava';
 import removeAccents from 'remove-accents';
 import DICTIONARY from '../static/DICTIONARY';
+import CARD_LIST from '../static/CARD_LIST';
+import ALIASES from '../static/ALIASES';
 import NAMES from '../static/NAMES';
 import { contains } from '../../dictionary';
+import generateDictionaryEntries from '../generateDictionaryEntries';
 
 test('Dictionary was created successfully', t => {
     t.truthy(DICTIONARY);
@@ -16,3 +19,31 @@ NAMES.forEach(name => {
         t.truthy(exists);
     });
 });
+
+// There should not be identical entries for different cards
+{
+    const dictEntries = generateDictionaryEntries(CARD_LIST, ALIASES);
+
+    function countEntries(
+        entries: Array<[string, CardID]>
+    ): { [string]: CardID[] } {
+        return entries.reduce((acc, [name, id]) => {
+            const existing = acc[name] || [];
+            const existingId = existing[0];
+            if (existingId !== id) {
+                acc[name] = existing.concat([id]);
+            }
+            return acc;
+        }, {});
+    }
+
+    const entryCounts = countEntries(dictEntries);
+
+    Object.keys(entryCounts).forEach(name => {
+        test(`No multiple matching cards for name: ${name} ${entryCounts[
+            name
+        ].join(' ')}`, t => {
+            t.is(entryCounts[name].length, 1);
+        });
+    });
+}
