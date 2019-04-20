88b8fcbe0bce007b6306d93e27cc9dfb55a38d3e
Soreine <soreine.plume@gmail.com>
2019-04-20 16:36:39 +0200

    Test that all listed cards in ALIASES exist

--- core/data/__tests__/DICTIONARY.js
+++ core/data/__tests__/DICTIONARY.js
@@ -3,6 +3,7 @@ import test from 'ava';
 import removeAccents from 'remove-accents';
 import DICTIONARY from '../static/DICTIONARY';
 import CARD_LIST from '../static/CARD_LIST';
+import CARDS from '../static/CARDS';
 import ALIASES from '../static/ALIASES';
 import { contains } from '../../dictionary';
 import generateDictionaryEntries from '../generateDictionaryEntries';
@@ -11,6 +12,7 @@ test('Dictionary was created successfully', t => {
     t.truthy(DICTIONARY);
 });
 
+// All cards are listed
 CARD_LIST.forEach(card => {
     test(`Dictionary contains  "${card.id}": "${card.name}"`, t => {
         const exists = contains(
@@ -22,9 +24,17 @@ CARD_LIST.forEach(card => {
     });
 });
 
+// No extra cards are listed
+Object.keys(ALIASES).forEach(id => {
+    test(`Card "${id}" still exists"`, t => {
+        const exists = CARDS[id];
+        t.truthy(exists);
+    });
+});
+
 // There should not be identical entries for different cards
 {
-    const dictEntries = generateDictionaryEntries(CARD_LIST, ALIASES);
+    const DICTIONARY_ENTRIES = generateDictionaryEntries(CARD_LIST, ALIASES);
 
     function countEntries(
         entries: Array<[string, CardID]>
@@ -39,7 +49,7 @@ CARD_LIST.forEach(card => {
         }, {});
     }
 
-    const entryCounts = countEntries(dictEntries);
+    const entryCounts = countEntries(DICTIONARY_ENTRIES);
 
     Object.keys(entryCounts).forEach(name => {
         test(`No multiple matching cards for name: ${name} ${entryCounts[
