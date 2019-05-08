a0360484ab7ec0883c7c87d847dd8e61516dc055
Soreine <soreine.plume@gmail.com>
2019-05-08 22:18:15 +0200

    Bump card version to 1.6

--- core/data/static/VERSION.js
+++ core/data/static/VERSION.js
@@ -4,7 +4,7 @@ import type { VersionJson } from '../../types';
 // Current version of the data
 const VERSION: VersionJson = {
     major: 1,
-    minor: 5
+    minor: 6
 };
 
 export default VERSION;
