f4113aaf9a3e64d813205d8d51211462fc7d8160
Soreine <hello@soreine.dev>
2019-09-02 12:51:21 +0200

    Lint

--- scripts/listNewCards.js
+++ scripts/listNewCards.js
@@ -8,12 +8,9 @@
 
 import RAW_CARDS from 'gwent-data-release/cards';
 
-import {
-    convertGwentDataReleaseCards,
-} from '../core/data';
+import { convertGwentDataReleaseCards } from '../core/data';
 import ALIASES from '../core/data/static/ALIASES';
 import IGNORED from '../core/data/static/IGNORED';
-
 
 function printAliasesEntry(card) {
     console.log(`'${card.id}': ['${card.name}'],`);
@@ -21,6 +18,8 @@ function printAliasesEntry(card) {
 
 const cards = convertGwentDataReleaseCards(RAW_CARDS, IGNORED);
 const cardList = Object.keys(cards).map(key => cards[key]);
-const missingCardsList = cardList.filter(card => !ALIASES[card.id] || IGNORED[card.id]);
+const missingCardsList = cardList.filter(
+    card => !ALIASES[card.id] || IGNORED[card.id]
+);
 
 missingCardsList.forEach(printAliasesEntry);
