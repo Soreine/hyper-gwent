66e8f328b9a1b79157ae81f5b108f471961a1a78
Soreine <hello@soreine.dev>
2019-09-02 12:50:20 +0200

    Make script for listing new cards

---
+++ scripts/listNewCards.js
@@ -0,0 +1,26 @@
+/* @flow */
+/* eslint-disable no-console */
+/*
+  Run with babel-node
+
+  Log the list of new (missing) cards to be added in ALIASES.js
+*/
+
+import RAW_CARDS from 'gwent-data-release/cards';
+
+import {
+    convertGwentDataReleaseCards,
+} from '../core/data';
+import ALIASES from '../core/data/static/ALIASES';
+import IGNORED from '../core/data/static/IGNORED';
+
+
+function printAliasesEntry(card) {
+    console.log(`'${card.id}': ['${card.name}'],`);
+}
+
+const cards = convertGwentDataReleaseCards(RAW_CARDS, IGNORED);
+const cardList = Object.keys(cards).map(key => cards[key]);
+const missingCardsList = cardList.filter(card => !ALIASES[card.id] || IGNORED[card.id]);
+
+missingCardsList.forEach(printAliasesEntry);
