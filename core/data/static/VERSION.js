ab0a71d75d4d6894b1328eaa3ba2301b17819ad3
Soreine <hello@soreine.dev>
2019-09-05 18:23:21 +0200

    Bump cards version to 1.7

--- core/data/static/VERSION.js
+++ core/data/static/VERSION.js
@@ -4,8 +4,8 @@ import type { VersionJson } from '../../types';
 // Current version of the data
 const VERSION: VersionJson = {
     major: 1,
-    minor: 6,
-    gwent: '2.1.1'
+    minor: 7,
+    gwent: '3.2.0'
 };
 
 export default VERSION;
